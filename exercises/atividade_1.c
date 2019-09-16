ebbe3267153a46f3cd7a8d337e498fde7fac7dfa
alanrslima <alanronison.lima@gmail.com>
2019-09-16 02:45:56 -0300

    Finalizado fluxo da atividade 1

--- exercises/atividade_1.c
+++ exercises/atividade_1.c
@@ -16,16 +16,24 @@ para referir a arvore:
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <time.h>
+#include <string.h>
+
+void removeLowercase (char *s);
+void removeVogals (char *s);
 
 int main()
 {
   clock_t t;
   double time_taken;
   t = clock();
-  int status;
+  int status, length;
+  char string[100];
   pid_t idProcesso; // Processo pai "P1"
   pid_t idChild;    // Identificador de filhos
-  printf("Sou o processo P1: %d\n", getpid());
+  printf("[LOG] Sou o processo P1: %d\n", getpid());
+
+  printf("Digite uma string de no máximo 100 caracteres: \n");
+  fgets(string, sizeof(string), stdin);
 
   idProcesso = fork();
 
@@ -33,14 +41,14 @@ int main()
     case -1: 
       exit(-1); // ERROR
     case 0: // Processo "P2"
-      printf("Sou o processo P2: %d  |  Meu pai P1: %d\n", getpid(), getppid());
+      printf("[LOG] Sou o processo P2: %d  |  Meu pai P1: %d\n", getpid(), getppid());
       idProcesso = fork();
       switch (idProcesso) {
         case -1:
           exit(-1); 
         case 0: // Processo "P4"
-          printf("Sou o processo P4: %d  |  Meu pai P2: %d\n", getpid(), getppid());
-          sleep(5);
+          removeLowercase(string);
+          printf("[LOG] Sou o processo P4: %d  |  Meu pai P2: %d\n", getpid(), getppid());
           break;
         default: // Continua em "P2"
           idProcesso = fork(); // Processo "P5"
@@ -48,8 +56,7 @@ int main()
             case -1:
               exit(-1); //ERROR
             case 0:  // P5
-              printf("Sou o processo P5: %d  |  Meu pai é P2: %d\n", getpid(), getppid());
-              sleep(5);
+              printf("[LOG] Sou o processo P5: %d  |  Meu pai é P2: %d\n", getpid(), getppid());
               break;
             default: // Continue P5
 
@@ -58,16 +65,16 @@ int main()
                 case -1:
                   exit(-1); // ERROR
                 case 0: // P6
-                  printf("Sou o processo P6: %d  |  Meu pai é P2: %d\n", getpid(), getppid());
-                  sleep(5);
+                  removeVogals(string);
+                  printf("[LOG] Sou o processo P6: %d  |  Meu pai é P2: %d\n", getpid(), getppid());
                   break;
                 default:
                   idChild = wait(&status);
-                  printf("Sou o processo P2: %d  |  Já esperei pelo meu filho: %d\n", getpid(), idChild);
+                  printf("[LOG] Sou o processo P2: %d  |  Já esperei pelo meu filho: %d\n", getpid(), idChild);
                   idChild = wait(&status); 
-                  printf("Sou o processo P2: %d  |  Já esperei pelo meu filho: %d\n", getpid(), idChild);
+                  printf("[LOG] Sou o processo P2: %d  |  Já esperei pelo meu filho: %d\n", getpid(), idChild);
                   idChild = wait(&status); 
-                  printf("Sou o processo P2: %d  |  Já esperei pelo meu filho: %d\n", getpid(), idChild);
+                  printf("[LOG] Sou o processo P2: %d  |  Já esperei pelo meu filho: %d\n", getpid(), idChild);
                   break;
               };
           };
@@ -79,31 +86,68 @@ int main()
         case -1:
           exit(-1); //ERROR
         case 0: // P3
-          printf("Sou o processo P3: %d  |  Meu pai é P1: %d\n", getpid(), getppid());
+          printf("[LOG] Sou o processo P3: %d  |  Meu pai é P1: %d\n", getpid(), getppid());
           idProcesso = fork(); // P3 cria filho P7
           switch (idProcesso) {
             case -1:
               exit(-1); //ERROR
             case 0:  // P7
-              printf("Sou o processo P7: %d  |  Meu pai é P3: %d\n", getpid(), getppid());
-              sleep(5);
+              printf("[LOG] Sou o processo P7: %d  |  Meu pai é P3: %d\n", getpid(), getppid());
+              length = strlen(string);
+              printf("- Tamanho da string informada dado por P7: %d\n", (length - 1));
               break;
             default: // Continua P3
                 idChild = wait(&status); // P3 espera por P7
-                printf("Sou o processo P3: %d  |  Já esperei pelo meu filho: %d\n", getpid(), idChild);
+                printf("[LOG] Sou o processo P3: %d  |  Já esperei pelo meu filho: %d\n", getpid(), idChild);
               break;
           }
           break;
         default: // Continua P1
           idChild = wait(&status);
-          printf("Sou o processo P1: %d  |  Já esperei pelo meu filho: %d\n", getpid(), idChild);
+          printf("[LOG] Sou o processo P1: %d  |  Já esperei pelo meu filho: %d\n", getpid(), idChild);
           idChild = wait(&status);
-          printf("Sou o processo P1: %d  |  Já esperei pelo meu filho: %d\n", getpid(), idChild);
-          printf("Sou o processo P1 de novo, meu id é: %d\n", getpid());
+          printf("[LOG] Sou o processo P1: %d  |  Já esperei pelo meu filho: %d\n", getpid(), idChild);
+          printf("[LOG] Sou o processo P1 de novo, meu id é: %d\n", getpid());
           t = clock() - t;
           time_taken = ((double)t) / CLOCKS_PER_SEC;
           printf("Tempo usado em segundos: %f\n", time_taken);
       };
       break;
   };
+};
+
+
+void removeLowercase (char *s){
+    int c, j = 0;
+    char newString[100];
+    while (s[c] != '\0'){
+        if ((s[c] >= 'a') && (s[c] <= 'z')) {
+            // É uma letra minuscula
+        } else {
+            newString[j] = s[c];
+            j++;
+        };
+        c++;
+    };
+    s = newString;
+    printf("- Removido caracteres minúsculos por P4: %s", newString);
+};
+
+
+void removeVogals (char *s){
+    int c, j = 0;
+    char newString[100];
+    while (s[c] != '\0'){
+        if ((s[c] == 'a') || (s[c] == 'A') || (s[c] == 'e') || (s[c] == 'E') || 
+        (s[c] == 'i') || (s[c] == 'I') || (s[c] == 'o') || (s[c] == 'O') ||
+        (s[c] == 'u') || (s[c] == 'U')) {
+            // É uma lvogal
+        } else {
+            newString[j] = s[c];
+            j++;
+        };
+        c++;
+    };
+    s = newString;
+    printf("- Removido vogais por P6: %s", newString);
 };
